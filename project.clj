961fbafc67eb8cdbbbff0005504a854bb2afd711
Kei Tsuji <ktsujister@gmail.com>
2014-09-06 19:21:50 +0900

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject incanter-contrib-distribution "0.1.0-SNAPSHOT"
+(defproject incanter-contrib-distribution "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter/incanter-core "1.5.5"]
+                 [org.apache.commons/commons-math3 "3.3"]])
