d237255f583b9575179d2896707a368d64b08df4
Kei Tsuji <ktsujister@gmail.com>
2014-09-06 22:53:39 +0900

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject incanter-contrib-distribution "0.1.2-SNAPSHOT"
+(defproject incanter-contrib-distribution "0.1.2"
   :description "Implement more distribution using commons-math."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
